c3c4a70cc3c04e8ca32432746a8e490c5c7a84d9
Krystian Domagala <krystian5250@gmail.com>
2022-05-30 11:51:42 +0100

    Created the list of meals as arrays

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Version control teset");
+//List of breakfasts
+const breakfasts = [
+    "Cereal; simply just put cereal in a bowl and add milk, yum.",
+    "Toast; two slices of break in a toaster, put some butter on it and maybe a cheeky slice of ham.",
+    "Toasties; very unhealthy but gets the job done, quick and easy.",
+    "Some of last nights dinner; I know you don't want it, but someone has to eat it."
+]
+
+//List of dinners
+const dinners = [
+    "Nandos chicken; easy to make, takes somew time but worth it.",
+    "Carbonara; you know you make the best ones, it will always taste good.",
+    "Burgers; who doesn't love a burger.",
+    "Salad; maybe you should have some healthy food once in a while.",
+    "Takeaway; expensive but it's the easiest and quickest option, requires no effort.",
+    "Salmon; chuck some pasta in there, make a nice sauce, healthy and always tastes good.",
+    "Pasta bake; because you ran out of ideas."
+]
+
+//List of deserts
+const deserts = [
+    "Doughnut; you still have some left over.",
+    "Takeaway; treat yourself, because you have had a hard day.",
+    "Pancakes; the ingredients are always there."
+]
