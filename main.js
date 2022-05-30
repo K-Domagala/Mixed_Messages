aecde694d3d67ff4b366a7bbcb42c712cd0260c5
Krystian Domagala <krystian5250@gmail.com>
2022-05-30 12:08:23 +0100

    Added a function to display the a randomly generated message

--- main.js
+++ main.js
@@ -1,30 +1,46 @@
 //List of breakfasts
 const breakfasts = [
-    "Cereal; simply just put cereal in a bowl and add milk, yum.",
-    "Toast; two slices of break in a toaster, put some butter on it and maybe a cheeky slice of ham.",
-    "Toasties; very unhealthy but gets the job done, quick and easy.",
-    "Some of last nights dinner; I know you don't want it, but someone has to eat it."
+    "cereal; simply just put cereal in a bowl and add milk, yum.",
+    "toast; two slices of break in a toaster, put some butter on it and maybe a cheeky slice of ham.",
+    "toasties; very unhealthy but gets the job done, quick and easy.",
+    "some of last nights dinner; I know you don't want it, but someone has to eat it."
 ]
 
 //List of dinners
 const dinners = [
-    "Nandos chicken; easy to make, takes somew time but worth it.",
-    "Carbonara; you know you make the best ones, it will always taste good.",
-    "Burgers; who doesn't love a burger.",
-    "Salad; maybe you should have some healthy food once in a while.",
-    "Takeaway; expensive but it's the easiest and quickest option, requires no effort.",
-    "Salmon; chuck some pasta in there, make a nice sauce, healthy and always tastes good.",
-    "Pasta bake; because you ran out of ideas."
+    "nandos chicken; easy to make, takes somew time but worth it.",
+    "carbonara; you know you make the best ones, it will always taste good.",
+    "burgers; who doesn't love a burger.",
+    "salad; maybe you should have some healthy food once in a while.",
+    "takeaway; expensive but it's the easiest and quickest option, requires no effort.",
+    "salmon; chuck some pasta in there, make a nice sauce, healthy and always tastes good.",
+    "pasta bake; because you ran out of ideas."
 ]
 
 //List of deserts
 const deserts = [
-    "Doughnut; you still have some left over.",
-    "Takeaway; treat yourself, because you have had a hard day.",
-    "Pancakes; the ingredients are always there."
+    "doughnut; you still have some left over.",
+    "takeaway; treat yourself, because you have had a hard day.",
+    "pancakes; the ingredients are always there."
 ]
 
+//Randomly chooses and element from a given array
 function chooseRand(array){
     let randIndex = Math.floor(Math.random()*array.length);
     return array[randIndex];
 }
+
+//Used to display a randomly generated message
+function displayMessage(){
+    //Assign a random meal to each variable
+    let breakfast = chooseRand(breakfasts);
+    let dinner = chooseRand(dinners);
+    let desert = chooseRand(deserts);
+
+    //Output messages using the randomlyu selected meals
+    console.log(`Today for breakfast you should have ${breakfast}`);
+    console.log(`And because you want to actually eat more, you should also have some dinner: ${dinner}`);
+    console.log(`Because you deserve it, for deser you should have ${desert}`);
+}
+
+displayMessage();
